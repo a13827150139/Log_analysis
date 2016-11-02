c435dccc2b2b86252a7374dd81fc636d6f1a29f8
cp-m <a13827150139@163.com>
2016-11-02 16:00:44 +0800

    去掉global

--- comm/start_log.py
+++ comm/start_log.py
@@ -10,14 +10,14 @@ import time
 import sqlite3
 
 sys.path.append(os.getcwd())
-start_size = 0
+
 
 def log(oldsize,fsize):
     """处理动态的日志"""
     if oldsize > fsize:
         #如果已处理字符比文件的字符大，表示日志文件已切割，重新开始匹配。
         start_size = 0
-        global start_size
+        return start_size
     with open(config.log_file, 'r') as r2:
         while oldsize < fsize:
             #从指定游标开始匹配
@@ -26,7 +26,7 @@ def log(oldsize,fsize):
             #返回最新游标
             oldsize = r2.tell()
             start_size = oldsize
-            global start_size
+            
             # 导入匹配规则
             pwd = ("%s|%s|%s|%s|%s" % tp)
 	    conn = sqlite3.connect(config.db_file_path)
@@ -48,6 +48,7 @@ def log(oldsize,fsize):
                         conn.execute(sql);
                         conn.commit()
 	    conn.close()
+	    return start_size
 def old_or_new():
     """获取文件大小"""
     if os.path.exists(config.log_file):
@@ -57,10 +58,10 @@ def old_or_new():
         return False
 
 def kais():
+    start_size = 0
     while True:
         #判断文件有没有改变，有改变则执行日志分析函数
         if start_size != old_or_new():
-            log(start_size, old_or_new())
-            print(start_size, old_or_new())
-        time.sleep(1)
-        print(1)
+            start_size=log(start_size, old_or_new())
+        else :
+           time.sleep(5)
